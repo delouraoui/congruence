b63b823fa51eef4c2efcb480bdc48874bd8d1b6b
El ouraoui <elouraou@informatique.univ-paris-diderot.fr>
2017-07-01 12:30:23 +0200

    delete

--- allrun.sh
+++
@@ -1,9 +0,0 @@
-
-echo "Options: $options"
-echo
-
-for name in /home/delour/Stage-2017/closure-congruence-HO/implem/exemple/tests/*.smt2; do
-   ./closure.native -smt2 $name;
-   echo $name;
-    echo "=====================================";
-done
